8d43b98f66bf53f381c661f9dfb7bd72ae3bbc59
John Whelchel <johnwhelchel@gmail.com>
2013-06-16 20:35:41 -0400

    Change front page to be simpler, with navbar showing course rankings. Removed "dev" urls and templates. Now just one, with develop branch.

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 import pce.views
+import bayesian.views
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
