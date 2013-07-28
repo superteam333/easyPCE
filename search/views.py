5cac53955391b30c4f218c06b93c9e1432f53ce9
Matt Haake <mhaak3@gmail.com>
2013-07-28 12:49:19 -0400

    Added cross search functionality for department, distribution, professor, day, and advice.

--- search/views.py
+++ search/views.py
@@ -20,47 +20,47 @@ def onlyMostRecent(course_set):
 		if (c.regNum != prevRegNum):
 			ids.append(c.id)
 			prevRegNum = c.regNum
-	return course_set.filter(id__in=ids)
+	return course_set.distinct().filter(id__in=ids)
 
-def coursesByDepartment(dept, course_set):
+def coursesByDepartment(depts, course_set):
 	if course_set is None or len(course_set) == 0:
 		course_set = Course.objects
 
 	try:
-		return course_set.filter(coursenum__dept=dept)
+		return course_set.filter(coursenum__dept__dept__in=depts)
+	except Exception as inst:
+		return HttpResponse("F***%s | %s" % (type(inst), inst))
+
+	return None
+
+def coursesByDistribution(das, course_set):
+	if course_set is None or len(course_set) == 0:
+		course_set = Course.objects
+
+	try:
+		return course_set.filter(da__in=das)
 	except Exception as inst:
 		return HttpResponse("F***%s %s" % (type(inst), inst))
 
 	return None
 
-def coursesByDistribution(da, course_set):
+def coursesByProfessor(profs, course_set):
 	if course_set is None or len(course_set) == 0:
 		course_set = Course.objects
 
 	try:
-		return course_set.filter(da__iexact=da)
+		return course_set.filter(profs__in=profs)
 	except Exception as inst:
 		return HttpResponse("F***%s %s" % (type(inst), inst))
 
 	return None
 
-def coursesByProfessor(prof, course_set):
+def coursesByDateTime(dateTime, course_set):
 	if course_set is None or len(course_set) == 0:
 		course_set = Course.objects
 
 	try:
-		return course_set.filter(profs=prof)
-	except Exception as inst:
-		return HttpResponse("F***%s %s" % (type(inst), inst))
-
-	return None
-
-def coursesByTime(time, course_set):
-	if course_set is None or len(course_set) == 0:
-		course_set = Course.objects
-
-	try:
-		return course_set.filter(lectureTime__icontains=time)
+		return course_set.filter(lectureTime__icontains=dateTime)
 	except Exception as inst:
 		return HttpResponse("F***%s %s" % (type(inst), inst))
 
@@ -71,32 +71,16 @@ def coursesByAdvice(advice, course_set):
 		course_set = Course.objects
 
 	try:
-		n = 0
-		tupleList = []
-		instances = course_set.all().order_by('regNum', '-year', '-semester')
-		if instances is not None and len(instances) > 0:
-			i = 0
-			while i < len(instances):
-				prevRegNum = instances[i].regNum
-				course = instances[i]
-
-				# get advice for all of the instances of a course
-				course_instances = []
-				while (i < len(instances) and instances[i].regNum == prevRegNum ):
-					course_instances.append(instances[i])
-					i = i + 1
-				course_advice = Advice.objects.filter(instance__in=course_instances, text__icontains=advice).exclude(text__icontains=("not")) 
-
-				if len(course_advice) != 0:
-					tupleList.append((course, course_advice))
-				i = i+1
-
-			tupleList.sort(key = lambda t: -len(t[1]))
-			return tupleList
-		return None
+		result = course_set.filter(advice__text__icontains=advice[0])
+		for i in range(1, len(advice)-1):
+			result = result | course_set.filter(advice__text__icontains=advice[i])
+		return result
 	except Exception as inst:
 		return HttpResponse("F***%s %s" % (type(inst), inst))
 		#pass
+
+def isAdvice(qAdvice):
+	return 
 
 def search(request):
 	if not settings.DEBUG:
@@ -116,65 +100,112 @@ def search(request):
 		user=User(netid=netid)
 		user.save()
 
+
+	all_depts = Department.objects.all().order_by('dept')
+
 	html = "<ul>"
-	#list = onlyMostRecent(coursesByDepartment(Department.objects.filter(dept__iexact='COS'), None))
-	#list = onlyMostRecent(coursesByDistribution("la", None))
-	#list = onlyMostRecent(Course.objects)
-	# list = coursesByDepartment(Department.objects.filter(dept__iexact='COS'), None)
-	# list = coursesByDistribution("qr", list)
-	# list = coursesByTime("T Th", list)
-	# list = onlyMostRecent(list)
-	#list = coursesByProfessor(Professor.objects.filter(id=313), None)
 
-	for blah in list:
-		html = html + "<li>" + blah.title + "</li>"
-	return HttpResponse(html)
+	czipped = []
+	pzipped = []
+	dzipped = []
 
-	if 'q' in request.GET and request.GET['q']:
-		q = request.GET['q']
-		words = q.split()
-		errors = "Errors:\n"
-		
-		czipped = []
-		pzipped = []
-		dzipped = []
-		
-		# Check for "text search" in something
-		if re.search('^\".*\"', q) is not None:
-			text = re.search('^\".*\"', q).group(0)
-			text = text[1:-1]
-			after = q[len(text)+2:]
-			inIndex = after.find('in ')
-			if inIndex != -1:
-				after = after[inIndex+3:]
-			if re.search("^[a-zA-Z]{2,3}", after) is not None:
-				# text search in a distribution area
-				try:
-					courseList = []
-					adviceList = []
-					instances = Course.objects.filter(da=after).order_by('regNum', '-year', '-semester')
-					sorted = coursesByAdvice(text, instances)
-					#return(sorted)
-					
-					if len(sorted):
-						links = []
-						for c, al in sorted:
-							ccnums = c.coursenum_set.all().order_by("dept__dept")
-							numString = '<a href="/courses/%s%s">%s' % (ccnums[0].dept.dept, ccnums[0].number, ccnums[0])
-							for li in range(1, len(ccnums)):
-								numString = "%s / %s" % (numString, ccnums[li])
-							numString = "%s: %s</a>" % (numString, c.title)
-							links.append(numString)
-							for a in al:
-								f = a.text.lower().find(text.lower())
-								a.text = '%s<span id="highlight">%s</span>%s' % (a.text[0:f], a.text[f:f+len(text)], a.text[f+len(text):])
-						courseList, adviceList = zip(*sorted)
-						res = zip(links,adviceList,courseList)
-						t = get_template('textsearch.html')
-						ds = Department.objects.all().order_by('dept')
-						html = t.render(Context({'results':res,'query':q, 'user':user, 'alldepts':ds}))
-						return HttpResponse(html)
-				except Exception as inst:
-					return HttpResponse("***%s %s" % (type(inst), inst))
-					#pass
-	return HttpResponse("hi")
+	courses = None
+
+	qAdvice = request.GET.getlist("advice")
+	qDepts = request.GET.getlist("depts")
+	qProfs = request.GET.getlist("profs")
+	qDist = request.GET.getlist("dist")
+	qDays = request.GET.getlist("days")
+
+	mostRecent = not (qAdvice and qAdvice[0])
+
+	# Departments
+	if qDepts and qDepts[0]:
+		for i in range(len(qDepts)):
+			qDepts[i] = qDepts[i][:3].upper()
+
+		if not courses and mostRecent:
+			courses = onlyMostRecent(coursesByDepartment(qDepts, courses))
+		else:
+			courses = coursesByDepartment(qDepts, courses)
+
+	# Professors
+	if qProfs and qProfs[0]:
+		profs = []
+		for i in range(len(qProfs)):
+			words = qProfs[i].split()
+			qs = Professor.objects.filter(lastname__icontains=words[-1])				
+			for i in range(1, len(words)-1):
+				qs = qs.filter(firstname__icontains=words[i])
+			set = Set([])
+			for p in qs:
+				if p not in set:
+					set.add(p)
+					profs.append(p)
+			qs = Professor.objects.filter(firstname__icontains=words[-1])
+			for i in range(1, len(words)-1):
+				qs = qs.filter(lastname__icontains=words[i])
+			for p in qs:
+				if p not in set:
+					set.add(p)
+					profs.append(p)
+		if profs:
+			if not courses and mostRecent:
+				courses = onlyMostRecent(coursesByProfessor(profs, courses))
+			else:
+				courses = coursesByProfessor(profs, courses)
+
+	# Distribution Areas
+	if qDist and qDist[0]:
+		das = []
+		for i in range(len(qDist)):
+			das.append(qDist[i].upper())
+
+		if not courses or mostRecent:
+			courses = onlyMostRecent(coursesByDistribution(das, courses))
+		else:
+			courses = coursesByDistribution(das, courses)
+
+	# Days
+	if qDays and qDays[0]:
+		days = ""
+		for d in qDays:
+			days = days + " " + d
+
+		if not courses and mostRecent:
+			courses = onlyMostRecent(coursesByDateTime(days, courses))
+		else:
+			courses = coursesByDateTime(days, courses)
+		#return HttpResponse(courses)
+
+	# Advice
+	if qAdvice and qAdvice[0]:
+		courses = onlyMostRecent(coursesByAdvice(qAdvice, courses))
+
+	# display the results
+	links = []
+	courses = courses.order_by('coursenum__dept__dept', 'coursenum__number', 'title')
+	for c in courses:
+		cnum = c.coursenum_set.all()[0]
+		dept = cnum.dept.dept
+		n = cnum.number        
+
+		nums = c.coursenum_set.all().order_by('dept__dept')
+		numString = "%s" % nums[0]
+		for a in range(1, len(nums)):
+			numString = "%s / %s" % (numString, nums[a])		
+
+		a = '<a href="/courses/%s%s">%s: %s</a>' % (dept, n, numString, c.title)
+		links.append(a)
+
+	czipped = zip(courses, links)
+
+	if (czipped):
+		t = get_template('search_results.html')
+		html = t.render(Context({'courses':czipped, 'profs':pzipped, 'depts':dzipped, 'query':"query", 'user':user, 'alldepts':all_depts}))
+		return HttpResponse(html)	
+
+	list = qDepts
+	
+	t = get_template('search_empty.html')
+	return HttpResponse(t.render(Context({'user':user, 'q':"query", 'alldepts':all_depts})))
